f3eeb780248280c0dd7a2021fa4e93260366a4cb
guibass96 <55796668+guibass96@users.noreply.github.com>
2021-04-21 13:37:09 -0300

    criação views e models

---
+++ SalesWebMVC/Controllers/DepartamentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SalesWebMVC.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+ 
