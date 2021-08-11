c18de672af0c6dc2ff0be3a4378ff4d9491c6b13
guibass96 <55796668+guibass96@users.noreply.github.com>
2021-08-10 22:04:36 -0300

    inclusão basica de vendedor

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Service;
 namespace SalesWebMVC.Controllers
 {
@@ -18,6 +19,17 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesWebMVC/Service/SellerService.cs
+++ SalesWebMVC/Service/SellerService.cs
@@ -17,5 +17,11 @@ namespace SalesWebMVC.Service
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
